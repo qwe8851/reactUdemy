fee47ca2e1b5eb902e4267586ea6dbba7833c902
qwe8851 <dgh07027@naver.com>
2023-03-09 23:36:06 +0900

    create ExpenseItem Component

---
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+
+function ExpenseItem() {
+    return (
+        <div>
+            <div>March 28th 2023</div>
+            <div>
+                <div>Car Insurance</div>
+                <div>$294.67</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
