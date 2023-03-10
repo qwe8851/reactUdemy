16bb5cc82998a19cc1453b1c52e312507a8e6933
qwe8851 <dgh07027@naver.com>
2023-03-10 23:53:33 +0900

    create ExpenseItem.css

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+    const expenseDate = new Date(2023, 3, 10);
+    const expenseTitle = 'Car Intsurance';
+    const expenseAmount = 294.67;
+
     return (
         <div className="expense-item">
-            <div>March 28th 2023</div>
+            <div>{expenseDate}</div>
             <div className="expense-item__description">
-                <div>Car Insurance</div>
-                <div className="expense-item__price">$294.67</div>
+                <div>{expenseTitle}</div>
+                <div className="expense-item__price">${expenseAmount}</div>
             </div>
         </div>
     );
