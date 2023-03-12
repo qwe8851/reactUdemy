cd167d12b14ba2d48cdce6ce40bca982f4635f8d
qwe8851 <dgh07027@naver.com>
2023-03-12 18:03:15 +0900

    create Card Component
    Card 컴포넌트로 하위 노드에 자동으로 card class 부여하기

---
+++ react-complete-guide/src/components/Card.js
@@ -0,0 +1,11 @@
+import './Card.css';
+
+function Card(props){
+    const classes = 'card ' + props.className; // 하위 속성들에 자동으로 card 클래스 부여 
+
+    return(
+        <div className={classes}>{props.children}</div>
+    );
+}
+
+export default Card;
