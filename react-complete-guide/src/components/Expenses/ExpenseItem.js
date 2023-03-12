c1d10d467305cba8b7ba5aa51055dfdc29f615f3
qwe8851 <dgh07027@naver.com>
2023-03-12 21:13:27 +0900

    remove node_modules

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -9,7 +9,7 @@ const ExpenseItem = (props) => {
     // let title = props.title;    //일반 변수는 컴포넌트 재랜더링을 해주는 트리거역할을 해주지 못하게 됨
     const [title, setTitle] = useState(props.title);   // 구조분해할당?
 
-    const clickHandler = ()=>{
+const clickHandler = ()=>{
         setTitle('Updated!');
         console.log(title);
         // useState는 비동기임. 그래서 title을 변경 후 콘솔에 출력하면 아직 바뀌지 전 값이 나옴
